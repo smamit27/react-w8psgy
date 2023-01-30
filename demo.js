c07bc56864165f27d0ce271493ed22c49f6552a3
smamit27 <smamit27@gmail.com>
2023-01-30 18:27:43 +0530

    Initial commit

---
+++ demo.js
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+  const data = [
+    {
+      indexData: 0,
+      name: 'a',
+    },
+    {
+      indexData: 1,
+      name: 'b',
+    },
+    {
+      indexData: 2,
+      name: 'c',
+    },
+  ];
+  const handleChange = (event, newValue) => {
+    console.log();
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          {data.map((d) => (
+            <Tab label={d.name} {...a11yProps(d.indexData)} />
+          ))}
+        </Tabs>
+      </Box>
+      {data.map((d) => (
+        <TabPanel value={value} index={d.indexData}>
+          {d.name}
+        </TabPanel>
+      ))}
+
+      {/* <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel> */}
+    </Box>
+  );
+}
